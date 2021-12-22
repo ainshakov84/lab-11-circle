91b6bdcd1a18da8dac7f7f9addf924d04a25be0c
Александр Иншаков <a.inshakov84@gmail.com>
2021-12-22 20:50:50 +0300

    Добавьте файлы проекта.

---
+++ Статические методы/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Статические_методы
+{
+    static class Сircle
+    {
+        static public double GetSquareСircle(double r)
+        { return r*r * Math.PI; }
+        static public double GetLengthСircle(double r)
+      { return 2 * Math.PI * r; }
+        static public double GetPointСircle(double x, double y)
+           
+        { return Math.Sqrt(Math.Pow((x-0),2)+Math.Pow((y-0),2)) ;}
+    }
+}
---
+++ Статические методы/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Статические_методы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите координату x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите координату y");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите значение радиуса");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("площадь круга = " + Сircle.GetSquareСircle(r));
+            Console.WriteLine("длина окружности = " + Сircle.GetLengthСircle(r));
+            
+            if (r > Сircle.GetPointСircle(x, y))
+            {
+                Console.WriteLine("точка принадлежит окружности");
+            }
+            else 
+                    {
+                Console.WriteLine("точка не принадлежит окружности");
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
